1763e916bd85f9939df4373ba7140dec53e842d2
Milos <61538921+MilosM33@users.noreply.github.com>
2022-08-06 13:42:46 +0200

    JWT Auth backend

--- eshop-backend/routes/api.php
+++ eshop-backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+Route::post('me', [AuthController::class, 'me']);
 Route::apiResource('/products', ProductController::class)->except(['create', 'edit']);
