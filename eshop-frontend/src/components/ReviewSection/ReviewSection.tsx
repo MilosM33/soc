f749da6d54fa250ac984817fad1a63e60f3254c5
Milos <61538921+MilosM33@users.noreply.github.com>
2022-08-16 20:16:43 +0200

    reviews skeleton

--- eshop-frontend/src/components/ReviewSection/ReviewSection.tsx
+++ eshop-frontend/src/components/ReviewSection/ReviewSection.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Review } from "../../services/Api/Review";
 import ReviewComponent, { IReviewProps } from "../Review/ReviewComponent";
+import Skeleton from "../Skeleton/Skeleton";
 export default function ReviewSection(props: any) {
   const [reviews, setReviews] = useState<IReviewProps[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     Review.getAll(props.slug).then((res) => {
       setReviews(res.data);
+      setLoading(false);
     });
   }, []);
 
@@ -19,15 +22,19 @@ export default function ReviewSection(props: any) {
         className="flex flex-col gap-9 md:w-3/4 lg:w-1/2
          mx-auto"
       >
-        {reviews.map((review, index) => (
-          <ReviewComponent key={index} {...review}></ReviewComponent>
-        ))}
-        {reviews.length === 0 && (
-          <div className="text-center">
-            <h3 className="text-2xl">No reviews yet</h3>
-            <h2 className="text-xl">Be first who reviews this product!</h2>
-          </div>
-        )}
+        {loading
+          ? Array(10)
+              .fill(0)
+              .map((_) => (
+                <section>
+                  <Skeleton type="text"></Skeleton>
+                  <Skeleton type="text"></Skeleton>
+                  <Skeleton type="title"></Skeleton>
+                </section>
+              ))
+          : reviews.map((review, index) => (
+              <ReviewComponent key={index} {...review}></ReviewComponent>
+            ))}
       </div>
     </div>
   );
