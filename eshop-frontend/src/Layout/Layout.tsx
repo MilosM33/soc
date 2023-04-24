03e824ec8bc319b46f9e07314c1fbfd3f8468953
Milos <61538921+MilosM33@users.noreply.github.com>
2023-04-24 17:12:51 +0200

    build

--- eshop-frontend/src/Layout/Layout.tsx
+++ eshop-frontend/src/Layout/Layout.tsx
@@ -3,18 +3,18 @@ import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
 import { ScrollRestoration } from "react-router-dom";
 export default function Layout(props: any) {
-  return (
-    <div className="App min-h-screen relative flex flex-col">
-      <div className="px-4 py-2 bg-red-400 text-primary text-center text-lg">
-        Doprava zadarmo pri objednávke od 100 Kč
-      </div>
+	return (
+		<div className="App min-h-screen relative flex flex-col">
+			<div className="px-4 py-2 bg-red-400 text-primary text-center text-lg">
+				Free shipping for orders over €100
+			</div>
 
-      <Header></Header>
-      <section className={"px-4 mt-[32px] " + props.className}>
-        {props.children}
-      </section>
-      <ScrollRestoration></ScrollRestoration>
-      <Footer></Footer>
-    </div>
-  );
+			<Header></Header>
+			<section className={"px-4 mt-[32px] " + props.className}>
+				{props.children}
+			</section>
+			<ScrollRestoration></ScrollRestoration>
+			<Footer></Footer>
+		</div>
+	);
 }
